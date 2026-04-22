951335dca4afeacedbaab85e979048482ceda920
Jeppeoberg <jeob1000@stud.ek.dk>
2026-04-22 11:13:49 +0200

    Tilføjet kolon til exceptions

--- src/main/java/AquariumSystem/exception/ValidationException.java
+++ src/main/java/AquariumSystem/exception/ValidationException.java
@@ -2,6 +2,6 @@ package AquariumSystem.exception;
 
 public class ValidationException extends RuntimeException {
     public ValidationException(String message) {
-        super("Det er ikke muligt at udføre den ønskede handling" + " " + message);
+        super("Det er ikke muligt at udføre den ønskede handling:" + " " + message);
     }
 }
